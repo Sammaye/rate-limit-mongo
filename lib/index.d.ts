575e7a3307e45cdd9a9a79d179ae5eb1045188dd
Sam Millman <sam.millman@googlemail.com>
2025-03-31 19:52:26 +0100

    WIP

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -7,6 +7,18 @@ declare module 'rate-limit-mongo' {
         constructor(options: Partial<MongoStoreOptions>);
 
         /**
+         * Returns a rate limit record for the given key.
+         * @param key
+         */
+        get(key: string): Promise<MongoStoreRateLimitRecord>;
+
+        /**
+         * Returns a prefixed key.
+         * @param key
+         */
+        prefixKey(key: string): string;
+
+        /**
          * Increments the value in the underlying store for the given key.
          * @param key The key to use as the unique identifier passed down from RateLimit.
          */
@@ -30,6 +42,11 @@ declare module 'rate-limit-mongo' {
         resetAll(): void;
     }
 
+    export interface MongoStoreRateLimitRecord {
+        totalHits: number;
+        resetTime: Date;
+    }
+
     export interface MongoStoreOptions {
 
         /**
@@ -45,28 +62,28 @@ declare module 'rate-limit-mongo' {
         /**
          * username for authentication in mongodb
          */
-        user: string;
+        user?: string;
 
         /**
          * password for authentication in mongodb
          */
-        password: string;
+        password?: string;
 
         /**
          * db name against which authenticate use. If not set db name from uri will be taken.
          */
-        authSource: string;
+        authSource?: string;
 
         /**
          * mongodb collection instance. Required if uri hasn't been set.
          */
-        collection: Collection;
+        collection?: Collection;
 
         /**
          * mongodb connection options. Allows to pass additional connection options to mongodb.
          * The default connection options are `useUnifiedTopology: true`, `useNewUrlParser: true`.
          */
-        connectionOptions: MongoClientOptions;
+        connectionOptions?: MongoClientOptions;
 
         /**
          * time period, in milliseconds, after which record will be reset (deleted).
@@ -75,18 +92,18 @@ declare module 'rate-limit-mongo' {
          * As a result, documents may remain in a collection during the period between the expiration of the document and the running of the background task.
          * See [mongodb ttl indexes doc](https://docs.mongodb.com/v3.6/core/index-ttl/#timing-of-the-delete-operation) for more information.
          */
-        expireTimeMs: number;
+        expireTimeMs?: number;
 
         /**
          * indicates whether expireDate should be reset when changed or not.
          * Defaults to `false`.
          */
-        resetExpireDateOnChange: boolean;
+        resetExpireDateOnChange?: boolean;
 
         /**
          * function that will be called if error happened during incr, decrement or resetKey methods. Defaults to `_.noop`.
          */
-        errorHandler: (err: any) => any;
+        errorHandler?: (err: any) => any;
 
         /**
          * defines whether create ttl index (on `expirationDate` field with `expireAfterSeconds: 0`) on collection or not.
@@ -94,7 +111,12 @@ declare module 'rate-limit-mongo' {
          * due to restricted db permissions (see [#15](https://github.com/2do2go/rate-limit-mongo/issues/15) for details).
          * Defaults to `true`.
          */
-        createTtlIndex: boolean;
+        createTtlIndex?: boolean;
+
+        /**
+         * Used to prefix all keys
+         */
+        prefix?: string;
     }
 
 }
